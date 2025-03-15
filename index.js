6e337beda9235ef0cc7f02cf60008fc265fd842d
shisirthakuri <shahishisir087@gmail.com>
2025-03-15 18:10:29 +0530

    adding zod

--- index.js
+++ index.js
@@ -58,35 +58,68 @@
 //  app.listen,(3000)
 
 
-const express= require("express")
-const app = express();
+// const express= require("express")
+// const app = express();
 
-app.get('/health-checkup',function(req,res){
-  //do health check here
-  const username = req.headers.username;
-  const password = req.headers.password;
-  const kidneyId= req.query.kidneyId;
+// app.get('/health-checkup',function(req,res){
+//   //do health check here
+//   const username = req.headers.username;
+//   const password = req.headers.password;
+//   const kidneyId= req.query.kidneyId;
 
-  if(username !="kamal" || password !="shahi"){
-    res.status(400).json({
-      "message":"something up with your inputs"
-    })
-    return
-  }
-    if (kidneyId !=1  && kidneyId !=2){
+//   if(username !="kamal" || password !="shahi"){
+//     res.status(400).json({
+//       "message":"something up with your inputs"
+//     })
+//     return
+//   }
+//     if (kidneyId !=1  && kidneyId !=2){
     
-    res.status(400).json({
-     " message":"something up with your inputs"
-    })
-    return
-  }
+//     res.status(400).json({
+//      " message":"something up with your inputs"
+//     })
+//     return
+//   }
   
-     //do something kidney here
-    res.json({
-      message:"your kidney is fine"
-    })
+//      //do something kidney here
+//     res.json({
+//       message:"your kidney is fine"
+//     })
   
 
 
+// });
+// app.listen(3000)
+
+
+//why need input validation
+
+const express = require("express")
+const app = express()
+const zod = require('zod')
+const schema = zod.array(zod.number())
+
+app.use(express.json())
+app.post('/health-checkup',function(req,res){
+  //do something kidney here
+  const kidneys = req.body.kidneys;
+  // const  kidneyLength = kidneys.length;
+
+  // 
+  const response = schema.safeParse(kidneys)
+    res.send({
+      response
+    })
+  
 });
+//Global catches
+
+
+// app.use(function(err,req,res,next){
+//   res.json({
+//     "message":"something up with your server"
+//   })
+// })
 app.listen(3000)
+
+
