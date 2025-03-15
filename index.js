3bdbec8d06ae0bd8e4bee7e1ff4583fa15d3a603
shisirthakuri <shahishisir087@gmail.com>
2025-03-15 12:08:40 +0530

    dump way to authenticatin and validation

--- index.js
+++ index.js
@@ -1,20 +1,92 @@
-const express = require("express")
-const bodyParser = require ("body-parser")
-const app = express()
-const port = 3000
+//  const express = require("express")
+//  const bodyParser = require ("body-parser")
+//  const app = express()
+// const port = 3000
 
   
 
-app.use(bodyParser.json());
+// appp.use(bodyParser.json());
 
-app.post('/backend',(req,res)=>{
-    const message = req.body.message
-    console.log(message)
-    res.json({
-        output:"2+2 is 4"
+//  app.post('/backend',(req,res)=>{
+//      const message = req.body.message
+//    console.log(message)
+//      res.json({
+//          output:"2+2 is 4"       
+//     })
+   
+//    })
+
+
+
+
+
+
+// app.listen(port,()=>{
+
+//     console.log(` sucussfull on port ${port}`)
+//  })
+
+
+
+
+// const express = require("express")
+// const app = express()
+//  const user=[{
+//     name: "jhon",
+//     kindeys:[{
+//         healthy:false
+
+//     }]
+//  }];
+
+//  app.get("/", function(req,res){
+//    const jhonKindey = user[0].kindeys;
+//   const numberOfkidney = jhonKindey.length
+//   let numberOfHealthyKidney = 0;
+//   for(let i =0; i<jhonKindey.length; i++){
+//    if(jhonKindey[i].healthy){
+//       numberOfHealthyKidney = numberOfHealthyKidney+1;
+//    }
+// }
+//  const numberOfUnHealthyKidney = numberOfkidney- numberOfHealthyKidney
+//  res.json({
+//    numberOfkidney,
+//    numberOfHealthyKidney,
+//    numberOfUnHealthyKidney
+//  })
+//  })
+//  app.listen,(3000)
+
+
+const express= require("express")
+const app = express();
+
+app.get('/health-checkup',function(req,res){
+  //do health check here
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const kidneyId= req.query.kidneyId;
+
+  if(username !="kamal" || password !="shahi"){
+    res.status(400).json({
+      "message":"something up with your inputs"
     })
-})
+    return
+  }
+    if (kidneyId !=1  && kidneyId !=2){
+    
+    res.status(400).json({
+     " message":"something up with your inputs"
+    })
+    return
+  }
+  
+     //do something kidney here
+    res.json({
+      message:"your kidney is fine"
+    })
+  
 
-app.listen(port,()=>{
-    console.log(` sucussfull on port ${port}`)
-})
+
+});
+app.listen(3000)
